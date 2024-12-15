d0edc32f9775efdddb7c9fe9cb45d3ba7d705218
vedanthnyk25 <vedanthnayak25@gmail.com>
2024-12-15 14:28:30 +0530

    finished-PostForm

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -9,17 +9,17 @@ function Select({
   return (
     <div className='w-full'>
         {label && <label htmlFor={id} className='inline-block mb-1 pl-1'></label>}
-       <select
-       {...props}
-       id= {id}
-       ref= {ref}
-       className={`px-3 py-2 rounded-lg bg-whitetext-black outline-none focus:bg-gray-50duration-200 border border-gray-200 w-full
+      <select
+      {...props}
+      id= {id}
+      ref= {ref}
+      className={`px-3 py-2 rounded-lg bg-whitetext-black outline-none focus:bg-gray-50duration-200 border border-gray-200 w-full
         ${className}`}>
 
             {options?.map((option)=>(
                 <option key={option} value={option}>{option}</option>
             ))}
-       </select>
+      </select>
     </div>
   )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -3,10 +3,14 @@ import Footer from "./Footer/Footer";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn"; 
 import Container from "./container/Container";
+import Select from "./Select.jsx";
+import RTE from "./RTE";
 export {
     Header,
     Footer,
     Logo,
     LogoutBtn,
     Container,
+    Select,
+    RTE,
 }
