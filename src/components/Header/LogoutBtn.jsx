e3619a982107f51162c2e220e13e188306a0a5a5
vedanthnyk25 <vedanthnayak25@gmail.com>
2025-01-24 22:07:25 +0530

    fixed ui updation on logout

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
-import { useDispatch } from 'react-redux'
-import authService from '../../appwrite/auth'
-import {logout} from '../../store/authSlice'
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import authService from '../../appwrite/auth';
+import { logout } from '../../store/authSlice';
+import { useNavigate } from 'react-router-dom';
 
 function LogoutBtn() {
-    const dispatch= useDispatch()
-    const logoutHandler= async ()=>{
-        authService.logout().then(()=>{
-          dispatch(logout())  
-        })
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const logoutHandler = async () => {
+    try {
+      await authService.logout(); // Logout from backend
+      dispatch(logout()); // Update Redux store
+      navigate('/login'); // Redirect to login page
+    } catch (error) {
+      console.error('Logout failed:', error);
     }
+  };
+
   return (
     <button
-    className='inline-black px-6 py-2 duration-200
-    hover:bg-blue-400 rounded-full'
-    onClick={logoutHandler}>
+      className="inline-black px-6 py-2 duration-200 hover:bg-blue-400 rounded-full"
+      onClick={logoutHandler}
+    >
       Logout
     </button>
-  )
+  );
 }
-export default LogoutBtn
+
+export default LogoutBtn;
